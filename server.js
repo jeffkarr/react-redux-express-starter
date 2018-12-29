fdc10e14fab702639760efde48f3020d62a93b5e
Jeff Karr <jkarr001@gmail.com>
2018-12-29 14:54:15 -0600

    modified package.json and server.js to resolve heroku deploy issues.

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 
 const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api/customers', (req, res) => {
   const customers = [
@@ -12,6 +18,14 @@ app.get('/api/customers', (req, res) => {
   res.json(customers);
 });
 
-const port = process.env.PORT || 5000;
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 
 app.listen(port, () => `Server running on port ${port}`);
